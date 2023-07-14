adc82b4bdb3e55cbdd14ad545ba5d52a2f197fbe
MohammedZAKa <mz8099413@gmail.com>
2023-07-14 12:51:46 -0700

    2-print alphapet lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0; i < 26; i++)
 	{
-		putchar(alpha[i]);
+		putchar(alph[i]);
 	}
 	putchar('\n');
 	return (0);
