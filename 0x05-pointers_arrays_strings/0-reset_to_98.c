7c00a8993937aad5caf49d325079851fbfdfe269
MohammedZAKa <mz8099413@gmail.com>
2023-07-25 08:52:23 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset to 98 - A function take poiner as parameter
+ * and rest it to 98
+ *
+ * @n : Input
+ *
+ * Return - Always 0 (sucess)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
