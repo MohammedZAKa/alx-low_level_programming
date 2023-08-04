856b04fee53c979a1f2911d715d3a4ec3992babe
MohammedZAKa <mz8099413@gmail.com>
2023-08-04 11:57:55 -0700

    mylib

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * Describtion - This fuction check 
+ * the .o files
+ *
+ * Return - Always return (0)
+*/
+int main(void)
+{
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
+}
